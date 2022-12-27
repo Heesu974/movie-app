2c52ee9cc4d37e9880f65816a154536c12becb3d
DESKTOP-V4BT0E4\cnt_24 <heesu.kang95@gmail.com>
2022-12-27 16:55:31 +0900

    map의 인자argument로 함수 넘겨서 prop 전달하기

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function Food({name, picture}) {
  
   return (
    <div>
-    <img src={picture}/>
+    <img src={picture} alt={picture}/>
     <h3>{name}</h3>
    </div>
   )
@@ -13,37 +13,39 @@ function Food({name, picture}) {
 
 const foodILike = [
   {
+    id: 1,
   name: "kimchi",
   image: 'https://www.beyondkimchee.com/wp-content/uploads/2022/04/Kimchi-Jjigae-thumbnail.jpg'
 },
 {
+  id: 2,
   name: "kimbab",
   image: 'https://thumbs.dreamstime.com/z/kimbab-asian-style-gimbap-kimbap-korean-dish-made-steamed-white-rice-bap-various-other-ingredients-rolled-gim-81838757.jpg'
 },
 {
+  id:3,
   name: "susui",
   image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Various_sushi%2C_beautiful_October_night_at_midnight.jpg/640px-Various_sushi%2C_beautiful_October_night_at_midnight.jpg'
 },
 ];
+
+function renderFood(dish) {
+  console.log(dish); 
+  //라고 했을 때, inspect element 하면, 
+  return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+}
+
+
 function App() {
 
   return (
     <div className="App">
         <h1>Hello</h1>
-          {foodILike.map((food, index) => (
-          
-    <Food key={index} name={food.name} picture={food.image}/>
-            
-          ))}
+         {foodILike.map(renderFood)}
     </div>
   );
 }
 
 export default App;
  
-// 위와 같이 우리는 갖고 있지 않은 데이터를 복사 붙여넣기 할 수 없습니다.
-//데이터가 이미 API에서 왔다고 생각해봤을 때, 
-//이는 웹사이트에 동적 데이터를 추가하는 방법이다.
-//이를 위해 foodILike라고 불리는 array 를 생성한다.
-//위의 json 형태를 > array로 가져오고 > food를 이름과 함께 렌더링 하는가? 
-//friends.map((friend) => friend + ':)')
+//map은 각 item 별로 function을 호출
