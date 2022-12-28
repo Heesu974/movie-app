ca24994428eec67bc65a3c7831ec627ca09622de
heesu.kang <ghfhd159@naver.com>
2022-12-28 11:27:13 +0900

    setTimeout을 이용해서 state 변경

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    return console.log("constructor executed");
-  }
   state = {
-    count: 0,
-  };
-  plus = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
+    isLoading: true,
+    movies: [],
   };
   componentDidMount() {
-    console.log("componentDidMount executed");
-  }
-  componentDidUpdate() {
-    console.log("componentDidUpdate executed");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    console.log("render executed");
+    const { isLoading } = this.state;
     return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.plus}>Plus</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <div>{isLoading ? <h1>Loading ... </h1> : <h1>We are ready</h1>}</div>
     );
   }
 }
-
 export default App;
 
-//react Class component 는 render말고도,
-//life cycle method를 갖는데, 이는 기본적으로 react가 component를 생성하거나, 없애는 방법이다.
-//component가 업데이트될 때, 호출되는 다른 function이 있다.
-
-//1. mounting
-//-먼저호출되는 function이 하나 있는데, constructor이다. (js에서 class를 만들 때 호출되는 function)
-//component가 mount될 때, component가 screen에 표시될 때, component가 너의 website에 들어갈 때, constructor를 호출한다. 그 다음 render 실행. 그 다름 componentDidMount 함수 실행.
-//componentDidMount는 hey this component is rendered at very first time.
-
-//2. updating
-//-state를 변경하면 그게 업데이트이다.
-
-//3. unMounting
+//setState 의 default 값은 위와 값이 state 객체로 미리 지정할 필요는 없지만, 이렇게 하는 것이 좋은 습관,
+//즉, setState의 argument 자리에서 새롭게 추가가 가능하다.
