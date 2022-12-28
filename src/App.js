ad664417e6d0d1df084e8179af2425383a7a57db
heesu.kang <ghfhd159@naver.com>
2022-12-28 10:53:30 +0900

    this about component life-cycle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    return console.log("constructor executed");
+  }
   state = {
     count: 0,
   };
@@ -10,7 +14,14 @@ class App extends React.Component {
   minus = () => {
     this.setState((current) => ({ count: current.count - 1 }));
   };
+  componentDidMount() {
+    console.log("componentDidMount executed");
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate executed");
+  }
   render() {
+    console.log("render executed");
     return (
       <div>
         <h1>The number is : {this.state.count}</h1>
@@ -23,9 +34,16 @@ class App extends React.Component {
 
 export default App;
 
-//minus = () => {
-//  this.state.count : - 1;
-//}
-// 이처럼 state를 직접 변경하지 마십시오. 내가 state를 직접 변경하려고 하면, react는 render function을 refresh 하지 않기 때문이다.
-//즉, react는 매범ㄴ state를 변경할 때 render function 을 호출해서 바꿔주길 원한다.
-//setState를 사용하지 않으면, 새 state와 함께 render function이 호출되지 않을 겁니다.
+//react Class component 는 render말고도,
+//life cycle method를 갖는데, 이는 기본적으로 react가 component를 생성하거나, 없애는 방법이다.
+//component가 업데이트될 때, 호출되는 다른 function이 있다.
+
+//1. mounting
+//-먼저호출되는 function이 하나 있는데, constructor이다. (js에서 class를 만들 때 호출되는 function)
+//component가 mount될 때, component가 screen에 표시될 때, component가 너의 website에 들어갈 때, constructor를 호출한다. 그 다음 render 실행. 그 다름 componentDidMount 함수 실행.
+//componentDidMount는 hey this component is rendered at very first time.
+
+//2. updating
+//-state를 변경하면 그게 업데이트이다.
+
+//3. unMounting
